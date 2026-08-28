3cea264c0c130e1f7643ade1eb0c6e8ddebb7430
jlcharly-code <jlcharly@gmail.com>
2026-08-28 11:58:29 -0400

    ajout url

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FileField(upload_to="project_images/", blank=True)
+    technology = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="project_images/")
+    url = models.URLField(blank=True, null=True)  # nouveau champ
