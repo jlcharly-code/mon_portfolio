c1e7b6f3c763b87cfdb3073077e2c062f173f84b
jlcharly-code <jlcharly@gmail.com>
2026-08-28 22:57:00 -0400

    Fix: DEBUG lit maintenant la variable d'environnement

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7p28#0mi0kk2n3p*2ut3fizkvk&mutrmg+vvt3&%6aa$bti9c9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS =  os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
